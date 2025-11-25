71fdc017f15f017f76074b02ac3112ef153fcbf4
naomiraulin <naomifaiza48@gmail.com>
2025-11-25 22:48:19 +0700

    Question.java

--- model/Question.java
+++ model/Question.java
@@ -1,5 +1,25 @@
 package model;
 
 public class Question {
+    private String text;
+    private String[] choices;
+    private String correctAnswer;
 
+    public Question(String text, String[] choices, String correctAnswer) {
+        this.text = text;
+        this.choices = choices;
+        this.correctAnswer = correctAnswer;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String[] getChoices() {
+        return choices;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
 }
